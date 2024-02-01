755f3751eb65998ec8a085c58eabf9b0dff83cd5
Ilelaboye Lekan <40622920+ilelaboye@users.noreply.github.com>
2024-02-01 16:53:33 +0100

    remove vue default codes, restructure code

---
+++ src/router/public/index.ts
@@ -0,0 +1,8 @@
+const publicRoutes = [
+  {
+    path: '/',
+    component: () => import('@/views/public/login.vue')
+  }
+]
+
+export default publicRoutes
---
+++ src/views/public/login.vue
@@ -0,0 +1,4 @@
+<template>
+  <h4>Hello worlf</h4>
+  <p>WE are just starting</p>
+</template>
